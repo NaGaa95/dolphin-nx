6be6f56387f2b7011605fed13252cb366a23fcad
naga <38852591+NaGaa95@users.noreply.github.com>
2026-09-16 18:08:54 +0200

    1.1.0

--- Source/Core/DolphinSwitch/CoverDownload.cpp
+++ Source/Core/DolphinSwitch/CoverDownload.cpp
@@ -19,7 +19,7 @@
 #include <vector>
 
 #ifndef DOLPHIN_SWITCH_RELEASE_VERSION
-#define DOLPHIN_SWITCH_RELEASE_VERSION "1.0.3-v2"
+#define DOLPHIN_SWITCH_RELEASE_VERSION "1.1.0"
 #endif
 
 namespace DolphinSwitch::CoverDownload
